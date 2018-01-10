15812c9ea5bac7e372f09cfdab398001533cd3a3
dwarakcheyyur <dwarak@cloudreign.in>
2018-01-10 17:21:59 +0530

    --

--- google-sheet.js
+++ google-sheet.js
@@ -60,10 +60,29 @@ $("#foo").submit(function(event){
 });
 */
 //test gsheet insert from web form
+
+/*
 var $form = $('form#test-form'),
     url = 'https://script.google.com/a/education.cloudreign.in/macros/s/AKfycbxHiX2zzZwsPJtNmsiCWgCG-GvjHNaQwClAeswvlUr5FgZRPZ4/exec'
 
 $('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject(),
+    success: function(msg){
+        console.log("success");
+    }
+  })
+})
+*/
+
+var $form = $('form.contactForm'),
+    url = 'https://script.google.com/a/education.cloudreign.in/macros/s/AKfycbxHiX2zzZwsPJtNmsiCWgCG-GvjHNaQwClAeswvlUr5FgZRPZ4/exec'
+
+$('#submit').on('click', function(e) {
   e.preventDefault();
   var jqxhr = $.ajax({
     url: url,
